882e6d7e0bbb0ef9d6693bb9405dc3265e847ac5
Alexandre Silva <alexix.dasilva@gmail.com>
2026-06-16 19:13:24 -0300

    criando comandos customizados

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,28 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import { faker } from '@faker-js/faker';
+
+Cypress.Commands.add('realizarCadastro', (dados = {}) => {
+
+    const usuario = {
+        nome: faker.name.fullName(),
+        email: faker.internet.email().toLowerCase(),
+        telefone: faker.phone.number('###########'),
+        nascimento: faker.date.birthdate({ min: 18, max: 65, mode: 'age' }).toLocaleDateString('pt-BR'),
+        genero: faker.helpers.arrayElement(['Masculino', 'Feminino', 'Outro']),
+        comentario: faker.lorem.sentence(),
+        senha: faker.internet.password(12, true),
+        termos: true,
+        ...dados
+    };
+
+    cy.get('[data-testid="input-nome"]').type(usuario.nome);
+    cy.get('[data-testid="input-email"]').type(usuario.email);
+    cy.get('[data-testid="input-telefone"]').type(usuario.telefone);
+    cy.get('[data-testid="input-nascimento"]').type(usuario.nascimento);
+    cy.get(`.genero-opcoes > :nth-child(${['Masculino', 'Feminino', 'Outro'].indexOf(usuario.genero) + 1})`).click();
+    cy.get('[data-testid="input-comentario"]').type(usuario.comentario);
+    cy.get('[data-testid="input-senha"]').type(usuario.senha);
+    if (usuario.termos) {
+        cy.get('[name="termos"]').click();
+
+    }
+});
